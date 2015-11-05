ce69c75b68f4f4a5edf843bbf5b3b5b5f44938af
Reid Blomquist <reid@theprogrampdx.com>
2015-11-04 20:49:20 -0800

    add to example config so i don't forget

--- lita_config_sample.rb
+++ lita_config_sample.rb
@@ -6,4 +6,6 @@ Lita.configure do |config|
   config.handlers.onewheel_forecast_io.api_key = 'yourforecastiokey'
   config.handlers.onewheel_forecast_io.api_uri = 'https://api.forecast.io/forecast/'
   config.handlers.onewheel_forecast_io.colors = true
+  config.handlers.onewheel_forecast_io.img_api_uri = 'https://api.imgur.com/3/'
+  config.handlers.onewheel_forecast_io.img_api_key = 'Client-ID <your-clientid>'
 end
